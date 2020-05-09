dc7cdd71ffdaf5bb5646967bd0c9e50b3559ccda
adrien gaultier <vibez@ctemplar.com>
2020-05-09 17:17:26 +0200

    first

---
+++ app/sketch.js
@@ -0,0 +1,76 @@
+
+const url = 'http://50760297.ngrok.io/paste';
+
+
+let myCanvas, video, button; 
+var input = document.querySelector('input[type=file]');
+
+input.addEventListener('change', onFileChange);
+function onFileChange(){
+  var file = input.files[0];
+  let imageData;
+  var reader = new FileReader();
+  
+  reader.onload = (e) => {
+    imageData = e.target.result;
+    const postData = {
+      "image": imageData
+    };
+
+    runmodel(postData);
+  }
+  reader.readAsDataURL(file);
+}
+
+
+function setup() {
+  myCanvas = createCanvas(640, 480);
+  myCanvas.parent('cam');
+  createBtn();
+
+  // Create video images from webcam
+  video = createCapture();
+  video.size(640, 480);
+  video.hide();
+}
+
+function draw() {
+  // Draw videos on the canvas
+  image(video, 0, 0);
+}
+
+// Create a button
+function createBtn() {
+  button = createButton('go');
+  button.parent('cam');
+  // When the button is clicked, call image2Txt function
+  button.mousePressed(onButtonClicked);
+  createElement('br');
+}
+
+function onButtonClicked(){
+  if (myCanvas && myCanvas.elt) {
+    const canvasElt = myCanvas.elt;
+    const imageData = canvasElt.toDataURL('image/jpeg', 1.0);
+    const postData = {
+      "image": imageData
+    };
+    runmodel(postData);
+}}
+
+function runmodel(payload) {
+    // Send HTTP Post request to Runway with image data, runway will return the image caption
+    httpPost(url, 'json', payload, (output) => {
+      if (output)  {
+        var myCanvas = document.getElementById('res');
+        var ctx = myCanvas.getContext('2d');
+        var img = new Image;
+        img.onload = function(){
+          myCanvas.width=img.width;
+          myCanvas.height=img.height;
+          ctx.drawImage(img,0,0,img.width,img.height);
+        };
+        img.src = output['image'];
+      }
+    })
+}
---
+++ server/serve.py
@@ -0,0 +1,55 @@
+import io
+import numpy as np
+from PIL import Image
+
+import time
+import logging
+import argparse
+
+import runway
+from runway.data_types import number, text, image, file
+import argparse
+
+def sample(model, img):
+    # Ensure imaize is under 1024
+    if img.size[0] > 1024 or img.size[1] > 1024:
+        img.thumbnail((1024, 1024))
+    return model.run(np.array(img)).convert("L")
+   
+      
+
+@runway.setup(options={})
+def setup(opts):
+    import basnet
+    return basnet
+
+@runway.command(name='paste',
+                inputs= { 'image': image },
+                outputs={ 'image': image })
+def paste(model,inputs):
+    start = time.time()
+    logging.info('generating mask...')
+    img = inputs['image']
+    if img.size[0] > 1024 or img.size[1] > 1024:
+        img.thumbnail((1024, 1024))
+    mask = sample(model,img)
+    logging.info(' > compositing final image...')
+    ref = inputs['image']
+    empty = Image.new("RGBA", ref.size, 0)
+    res = Image.composite(ref, empty, mask.resize(ref.size))
+    # Print stats
+    logging.info(f'Completed in {time.time() - start:.2f}s')
+    return res
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ngrok', action='store_true')
+    parser.add_argument('--port', default=8081,type=int)
+    logging.basicConfig(level=logging.INFO)
+    args = parser.parse_args()
+    if args.ngrok:
+        from pyngrok import ngrok
+        public_url = ngrok.connect(args.port)
+        logging.info(" * ngrok tunnel \"{}\" -> \"http://127.0.0.1:{}/\"".format(public_url, args.port))
+    runway.run(host='0.0.0.0', port=args.port)
